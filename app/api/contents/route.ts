7eb20daf55d9135e6c622a9ada0e92026cee5132
adimiuprix <miuprix@gmail.com>
2026-02-18 14:50:56 +0700

    feat: add API route to fetch content from the backend service.

--- app/api/contents/route.ts
+++ app/api/contents/route.ts
@@ -1,16 +1,18 @@
 export const runtime = 'edge';
 
 type ApiError = {
-  status: false
-  message: string
+    status: false
+    message: string
 }
 
 export async function GET(): Promise<Response> {
     try {
-        const res = await fetch('https://tascen.site/api/content', {
-            cache: 'no-cache',
-            // next: { revalidate: 60 },
-        })
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_APP}/api/content`,
+            {
+                cache: 'no-store',
+            }
+        )
 
         if (!res.ok) {
             return Response.json(
@@ -22,8 +24,8 @@ export async function GET(): Promise<Response> {
         return Response.json(await res.json())
     } catch (e) {
         return Response.json(
-        { status: false, message: e instanceof Error ? e.message : 'Fetch error' },
-        { status: 500 }
+            { status: false, message: e instanceof Error ? e.message : 'Fetch error' },
+            { status: 500 }
         )
     }
 }
